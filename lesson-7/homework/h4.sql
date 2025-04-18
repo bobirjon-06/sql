f1e09e2050da5e59f086951bcba9263abbb8a149
Bobirjon Axmadjonv <bahmadjonov4@gmail.com>
2025-04-18 11:55:34 +0500

    done

---
+++ lesson-7/homework/h4.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS OrderDetails;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Products;
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE
+);
+
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
+    ProductID INT,
+    Quantity INT,
+    Price DECIMAL(10,2)
+);
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50)
+);
+
+INSERT INTO Customers (CustomerID, CustomerName) VALUES
+(1, 'Alice'),
+(2, 'Bob'),
+(3, 'Charlie'),
+(4, 'David');
+INSERT INTO Orders (OrderID, CustomerID, OrderDate) VALUES
+(101, 1, '2025-04-01'),
+(102, 2, '2025-04-05'),
+(103, 1, '2025-04-10');
+INSERT INTO OrderDetails (OrderDetailID, OrderID, ProductID, Quantity, Price) VALUES
+(1, 101, 1, 2, 25.00),
+(2, 102, 2, 1, 15.00),
+(3, 103, 1, 3, 30.00);
+
+
+SELECT c.CustomerName, COUNT(o.OrderID) AS OCount
+FROM Customers c
+Join Orders o ON o.CustomerID=c.CustomerID
+group by c.CustomerID, c.CustomerName
+having count(o.OrderID)>1;
---
+++ lesson-7/homework/h5.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS OrderDetails;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Products;
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE
+);
+
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
+    ProductID INT,
+    Quantity INT,
+    Price DECIMAL(10,2)
+);
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50)
+);
+
+INSERT INTO Customers (CustomerID, CustomerName) VALUES
+(1, 'Alice'),
+(2, 'Bob'),
+(3, 'Charlie'),
+(4, 'David');
+INSERT INTO Orders (OrderID, CustomerID, OrderDate) VALUES
+(101, 1, '2025-04-01'),
+(102, 2, '2025-04-05'),
+(103, 1, '2025-04-10');
+INSERT INTO OrderDetails (OrderDetailID, OrderID, ProductID, Quantity, Price) VALUES
+(1, 101, 1, 2, 25.00),
+(2, 102, 2, 1, 15.00),
+(3, 103, 1, 3, 30.00);
+
+
+
+SELECT o.OrderID, p.ProductName, od.Price
+FROM Orders o
+JOIN OrderDetails od ON o.OrderID = od.OrderID
+JOIN Products p ON od.ProductID = p.ProductID
+WHERE od.Price = (
+	SELECT MAX(Price)
+	FROM OrderDetails
+	Where OrderId = o.OrderID
+);
